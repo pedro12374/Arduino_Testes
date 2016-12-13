265dd0b152bd288aa77f085182fd0ed4b05d1d98
pedro12374 <pedrohaerter@hotmail.com>
2016-12-13 14:23:24 -0200

    Sensor de Luz

---
+++ Sensor_Luz/Sensor_Luz.ino
@@ -0,0 +1,32 @@
+/*
+Projeto Arduino sensor de luz.
+Por Jota
+----------------------------------------
+--=<| www.ComoFazerAsCoisas.com.br |>=--
+----------------------------------------
+*/
+ 
+//Pino analógico em que o sensor está conectado.
+int sensor = 0;      
+ 
+//variável usada para ler o valor do sensor em tempo real.
+int valorSensor = 0; 
+ 
+//Método setup, executado uma vez ao ligar o Arduino.
+void setup(){
+  //Ativando o serial monitor que exibirá os 
+  //valores lidos no sensor.
+  Serial.begin(9600);
+}
+ 
+//Método loop, executado enquanto o Arduino estiver ligado.
+void loop(){
+   
+  //Lendo o valor do sensor.
+  int valorSensor = analogRead(sensor);
+   
+  //Exibindo o valor do sensor no serial monitor.
+  Serial.println(valorSensor);
+   
+  delay(2000); 
+}
